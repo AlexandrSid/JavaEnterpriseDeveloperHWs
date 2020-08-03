b981204d011a036dfad65d23ea02051b07da2abd
AlexandrSid <alexandr.sidorkin@gmail.com>
2020-08-03 13:01:17 +0300

    3 of 3 done

---
+++ src/com/company/lesson3/personsorter/Sex.java
@@ -0,0 +1,16 @@
+package com.company.lesson3.personsorter;
+
+public enum Sex {
+    MAN(1),
+    WOMAN(0);
+
+    public int getNumerical() {
+        return numerical;
+    }
+
+    private final int numerical;
+
+    Sex(int i) {
+        this.numerical = i;
+    }
+}
