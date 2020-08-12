d002d077182d91d2eb171227eebe9a626862f45a
AlexandrSid <alexandr.sidorkin@gmail.com>
2020-08-12 13:34:26 +0300

    2nd block finished. Side branches merged into master. Project structure prepared for lesson6 and lesson7 HWs

---
+++ src/main/java/com/alexsid/lesson3/numbergenerator/NumberGenerator.java
@@ -0,0 +1,43 @@
+package main.java.com.alexsid.lesson3.numbergenerator;
+
+import java.util.Scanner;
+
+/**2. Составить программу, генерирующую N случайных чисел.
+ * Для каждого числа k вычислить квадратный корень q.
+ * Если квадрат целой части q числа равен k, то вывести это число на экран.
+ * Предусмотреть что первоначальные числа могут быть отрицательные, в этом случае генерировать исключение.
+ */
+ public class NumberGenerator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число N");
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            try {
+                double k = Math.random() * 20 - 5;//от -5 до 15
+                if(k < 0) {
+                    throw new NumberIsLowerThanZeroException(k);
+                }
+                int q = (int)Math.sqrt(k);
+                if (q*q == (int)k){
+                    System.out.println(k);
+                }
+            } catch (NumberIsLowerThanZeroException e) {
+                System.out.format("Число оказалось отрицательным: %f%n",e.getReason());
+            }
+        }
+    }
+
+    private static class NumberIsLowerThanZeroException extends RuntimeException {
+
+        public double getReason() {
+            return reason;
+        }
+
+        private final double reason;
+
+        public NumberIsLowerThanZeroException(double random) {
+            this.reason = random;
+        }
+    }
+}
---
+++ src/main/java/com/alexsid/lesson3/personsorter/FastPersonSorter.java
@@ -0,0 +1,45 @@
+package main.java.com.alexsid.lesson3.personsorter;
+
+import java.util.Comparator;
+import java.util.Random;
+
+public class FastPersonSorter implements PersonSorter {
+    @Override
+    public void sort(Person[] persons) {
+        quicksort(persons, 0, persons.length - 1);
+
+    }
+
+    Random pivotRng = new Random();
+
+
+    private <T> void quicksort(T[] items, int left, int right) {
+        if (left < right) {
+            int pivotIndex = pivotRng.nextInt(right - left) + left;
+            int newPivot = partition(items, left, right, pivotIndex);
+
+            quicksort(items, left, newPivot - 1);
+            quicksort(items, newPivot + 1, right);
+        }
+    }
+
+    private <T> int partition(T[] items, int left, int right, int pivotIndex) {
+        Comparator comparator = new PersonComparator();
+
+        T pivotValue = items[pivotIndex];
+
+        swap(items, pivotIndex, right);
+
+        int storeIndex = left;
+
+        for (int i = left; i < right; i++) {
+            if (comparator.compare(items[i], pivotValue) < 0) {
+                swap(items, i, storeIndex);
+                storeIndex += 1;
+            }
+        }
+
+        swap(items, storeIndex, right);
+        return storeIndex;
+    }
+}
