c8e2a68d1d46175f0918614bf369a0a1b09accc9
AlexandrSid <alexandr.sidorkin@gmail.com>
2020-09-14 01:15:14 +0300

    Всё. это не работает. может когда-то раньше jre поволяла подменять классы, но теперь видать безопаснее стала. Исходник меняется, компилируется, но до перезапуска программы либо не влияет на её работу, либо ломает машину.
    Зато узнал про  NoClassDefFoundError

--- src/main/java/com/alexsid/lesson10/task4/SomeClass.java
+++ src/main/java/com/alexsid/lesson10/task4/SomeClass.java
@@ -2,9 +2,15 @@ package main.java.com.alexsid.lesson10.task4;
 
 public class SomeClass implements Worker {
     static {
-        System.out.println("SomeClass loaded with " + SomeClass.class.getClassLoader().getName());
+        System.out.println("SomeClass loaded with " + SomeClass.class.getClassLoader());
     }
     @Override
     public void doWork() {
+System.out.println("QQY");
+System.out.println("QQT");
+System.out.println("QQR");
+System.out.println("QQE");
+System.out.println("QQW");
+System.out.println("QQQ");
     }
 }
