7ca157d8ecae5caade204b8f98221f3a6598c7a9
AlexandrSid <alexandr.sidorkin@gmail.com>
2020-10-27 15:48:54 +0300

    pet ID type changed to long, now generated with UUID.randomUUID();

--- src/main/java/com/alexsid/lesson17/pets/PetFactory.java
+++ src/main/java/com/alexsid/lesson17/pets/PetFactory.java
@@ -2,19 +2,20 @@ package com.alexsid.lesson17.pets;
 
 import com.alexsid.lesson17.Person;
 
+import java.util.UUID;
+
 public class PetFactory {
 
     private static PetFactory factory = new PetFactory();
-    private int idKeeper = 0;
 
     public Pet createPet(String petType, Person owner, String name, Double weight) {
-        int newId = idKeeper++;
-
+        int newId =
+                UUID.randomUUID().clockSequence();
         return switch (petType.toLowerCase()) {
-            case "cat" ->    new Cat(newId, name, owner, weight);
-            case "dog" ->    new Dog(newId, name, owner, weight);
+            case "cat" -> new Cat(newId, name, owner, weight);
+            case "dog" -> new Dog(newId, name, owner, weight);
             case "parrot" -> new Parrot(newId, name, owner, weight);
-            default ->       new Exotic(newId, name, owner, weight);
+            default -> new Exotic(newId, name, owner, weight);
         };
     }
 
