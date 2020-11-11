c53629eab81f3b6dc82b2d88ab68de1b8f211fe1
AlexandrSid <alexandr.sidorkin@gmail.com>
2020-11-12 00:15:49 +0300

    Hi разложен на M V и C, использовался lombok

---
+++ src/main/java/com/alexsid/lesson24/ModelHi.java
@@ -0,0 +1,13 @@
+package com.alexsid.lesson24;
+
+import lombok.Data;
+
+@Data
+public class ModelHi {
+
+    private String name;
+    private String buttonText;
+    private String panelHeader;
+    private String displayedMessage;
+
+}
