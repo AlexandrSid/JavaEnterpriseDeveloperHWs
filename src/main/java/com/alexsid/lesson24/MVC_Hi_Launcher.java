415e17e2b52c1c136934fa9ca94587b974aa6d65
AlexandrSid <alexandr.sidorkin@gmail.com>
2020-11-11 23:40:41 +0300

    Hi сделан MVC, применён lombok

---
+++ src/main/java/com/alexsid/lesson24/MVC_Hi_Launcher.java
@@ -0,0 +1,14 @@
+package com.alexsid.lesson24;
+
+public class MVC_Hi_Launcher {
+
+    public static void main(String[] args) {
+        ModelHi model = new ModelHi();
+            model.setName("");
+            model.setButtonText("Say Hello");
+            model.setPanelHeader("New MVC-shaped application");
+            model.setDisplayedMessage("<= Press this button>");
+
+        new ControllerHi(model);
+    }
+}
--- src/main/java/com/alexsid/lesson24/ViewHi.java
+++ src/main/java/com/alexsid/lesson24/ViewHi.java
@@ -1,4 +1,44 @@
 package com.alexsid.lesson24;
 
-public class ViewHi {
+import lombok.Getter;
+
+import javax.swing.*;
+
+@Getter
+public class ViewHi extends JFrame{
+    private JPanel panel;
+    private JTextField userInputTextField;
+    private JButton sayButton;
+    private JLabel displayMessage;
+
+    public ViewHi(String displayMessage, String panelHeader, String buttonText) {
+        this.panel = new JPanel();
+        this.userInputTextField = new JTextField(20);
+        this.sayButton = new JButton(buttonText);
+        this.displayMessage = new JLabel(displayMessage);
+
+        panel.add(new JLabel("Enter your name:"));//в самом конце вспомнил((
+
+        panel.add(userInputTextField);
+
+        panel.add(sayButton);
+
+        panel.add(this.displayMessage);
+
+        // Add a mouse listener to the button
+
+//        sayButton.addMouseListener(new ControllerHi());//TODO move to controller
+
+        this.setContentPane(panel);
+
+        this.pack();
+
+        this.setTitle(panelHeader);
+
+        // The window closing event should probably be passed to the
+
+        // Controller in a real program, but this is a short example.
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
